a548ecb0804954cff8c89aa47dd85e4001222520
Ross Fisher <ubercomputers@gmail.com>
2026-02-23 19:31:56 -0500

    Initial Commit

---
+++ FamilySafe-NSFW-Finder/FamilySafe_NSFW_FinderApp.swift
@@ -0,0 +1,17 @@
+//
+//  FamilySafe_NSFW_FinderApp.swift
+//  FamilySafe-NSFW-Finder
+//
+//  Created by Ross Fisher on 2/23/26.
+//
+
+import SwiftUI
+
+@main
+struct FamilySafe_NSFW_FinderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
